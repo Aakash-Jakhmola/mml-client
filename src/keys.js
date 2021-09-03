4a2af2cdae4037f1b32182404f3ec7df2e6da9cb
Aakash Jakhmola <58803066+Aakash-Jakhmola@users.noreply.github.com>
2021-09-03 21:18:09 +0530

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,4 +1,4 @@
-// const base_url = "https://aqueous-springs-48458.herokuapp.com/"
-const base_url = "http://localhost:8000/"
+ const base_url = "https://aqueous-springs-48458.herokuapp.com/"
+// const base_url = "http://localhost:8000/"
 
 export default base_url
